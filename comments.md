0322a2763e3ab8e27d10c868c5e087474a362912
ZhaoZhang <zhaozhang@thoughtworks.com>
2020-10-28 15:56:06 +0800

    add comments

---
+++ comments.md
@@ -0,0 +1,39 @@
+### 完成度：
+* 只实现了分组的核心逻辑，没有提供任何接口
+* 完成度太低，其它维度无法正常考察
+
+__Details:__
+
+
+
+### 测试：
+* 有少量测试
+
+__Details:__
+
+
+
+### 知识点：
+* 需要了解下三层架构
+* 缺少Controller类
+
+__Details:__
+
+- \- 违反三层架构，存储数据的类应该命名为xxxRepository,并使用@Repositoy注解
+- \- 需要掌握Spring MVC相关注解
+- \- Service应该使用@Service注解
+
+### 工程实践：
+* 违反面向对象，Member和Group没有对应的类
+
+__Details:__
+
+
+
+### 综合：
+
+
+__Details:__
+
+
+
--- src/main/java/com/thoughtworks/capability/gtb/entrancequiz/GtbEntranceQuizApplication.java
+++ src/main/java/com/thoughtworks/capability/gtb/entrancequiz/GtbEntranceQuizApplication.java
@@ -6,9 +6,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 // TODO GTB-知识点: * 需要了解下三层架构
 // TODO GTB-知识点: * 缺少Controller类
 // TODO GTB-完成度: * 只实现了分组的核心逻辑，没有提供任何接口
-// TODO GTB-知识点: * 违反面向对象，Member和Group没有对应的类
+// TODO GTB-工程实践: * 违反面向对象，Member和Group没有对应的类
 // TODO GTB-完成度: * 完成度太低，其它维度无法正常考察
 // TODO GTB-知识点: - 需要掌握Spring MVC相关注解
+// TODO GTB-测试: * 有少量测试
 @SpringBootApplication
 public class GtbEntranceQuizApplication {
 
